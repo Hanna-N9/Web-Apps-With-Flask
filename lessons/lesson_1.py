59f60911a68a80769b9d2b9f4c09a75f1088d169
Hanna Negash <hnegash.n@gmail.com>
2024-06-19 20:42:42 -0700

    Add a brief explaination of decorators

--- lessons/lesson_1.py
+++ lessons/lesson_1.py
@@ -1,2 +1,9 @@
 # Reference from the video, https://www.youtube.com/watch?v=WOHsHaaJ8VQ based on Decorators in Python
 
+# Decorators allow programmers to wrap a function or method with additional functionality without altering its source code. 
+# Decorators are special functions which can be used to return functionality to function. 
+# Decorators can be applied using the @ symbol followed by the decorator name before the function definition. 
+# This is to make code cleaner and readable.
+
+# All functions are objects
+    # 0:46
