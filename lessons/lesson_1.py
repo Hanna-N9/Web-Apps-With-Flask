f26b40324cc383d8b722fa8ebf4b4def6b445633
Hanna Negash <hnegash.n@gmail.com>
2024-06-23 21:54:42 -0700

    Add a comment to briefly explain what the numbers represent

--- lessons/lesson_1.py
+++ lessons/lesson_1.py
@@ -3,5 +3,7 @@
 # Decorators allow programmers to wrap a function with another function) without modifying its source code. Decorators can be applied using
 # the @ symbol followed by the decorator name before the function definition. This is to make code cleaner and readable.
 
-# All functions are objects
+# Six different steps/ways to use functions and then finally, in the end, to use decorators: 
+
+# 1. All functions are objects
     # 0:46
